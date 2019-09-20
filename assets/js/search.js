db59b5a37570fe9c7e1bd286be6e7a6e6b902957
Merlin-R <Merlin-R@users.noreply.github.com>
2019-09-20 23:22:53 +0000

    Deploy to GitHub pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable"},"rows":[{"id":0,"kind":32,"name":"EMBLAZON","url":"globals.html#emblazon","classes":"tsd-kind-variable"}]};
